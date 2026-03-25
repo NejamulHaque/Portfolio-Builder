e849f58431611eeb658bafe8802223f04c7cbc71
Nejamul Haque <nejamulhaque.05@gmail.com>
2026-03-25 19:33:00 +0530

    Secure API keys by moving them to environment variables

--- client/src/firebase/config.js
+++ client/src/firebase/config.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCrw2y8TfVf7OlYMVuygYXiOJ6MZgjQkec",
-  authDomain: "portfolio-builderr.firebaseapp.com",
-  projectId: "portfolio-builderr",
-  storageBucket: "portfolio-builderr.firebasestorage.app",
-  messagingSenderId: "487334111173",
-  appId: "1:487334111173:web:db1308967b37b7c64bcdac",
-  measurementId: "G-V9KGSFELLF"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
